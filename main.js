0a0779be777dcbe81c0f218c2343e137539e24ec
leandro <romeroleandro0711@gmail.com>
2023-08-06 18:21:43 -0400

    add main.js

--- main.js
+++ main.js
@@ -24,23 +24,27 @@ const renderMealCatagories = (mealCatagoriesObj) => {
     console.log('renderMealCatagories');
     console.table('focus ocurred');
 
-    //find and select a dom element to append my data into
+//find and select a UL with meal catagory dom element to append my data into
+//
 
-// for each element in our meal catagories array 
-  //create a wrapping element maybe <article> class of card 
-  //append the article,card to DOM 
-  //display the catagory name, image, and description
-    //image
-    // create an img element
-     //set img src to catagory thumbail url
-  // give class of catagory-thumbail
-  //give it a mobile friendly max width of like 300px
-  //name
-  //create an h4 element
-  // set h4 element textContent to be meal catagory name
-  //description
-  //create an p element
-        // set p element textContent to be meal catagory description
+//for each element in our meal catagories array 
+    //create a wrapping element maybe <article> class of card 
+    //append the article,card to selected DOM Element
+    //display the catagory name, image, and description
+        //image
+            // create an img element
+            //set img src to catagory thumbail url
+            // give class of catagory-thumbail
+            //give it a mobile friendly max width of like 300px
+            //appendChild the image to our card
+        //name
+            //create an h4 element
+            // set h4 element textContent to be meal catagory name
+            //appendChild the h4 to our card
+        //description
+            //create an p element
+            // set p element textContent to be meal catagory description
+            //appendChild the p to our card
 };
 
 // funtion to call when form inputs is given focus
