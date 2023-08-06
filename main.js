7a54b22d941a57125d3b429baee614cc55bb4cc2
leandro <romeroleandro0711@gmail.com>
2023-08-06 15:25:08 -0400

    add main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-console.log('main.js connected')
+console.log('main.js connected');
 
-const handleFormInputFocus = () => {
-console.log('focus occurred')
+const searchTermsInputs = document.body.querySelector("#search-terms");
+
+const getMealCatagories = async () =>{
+const mealCatagoriesApiURL ="https://www.themealdb.com/api/json/v1/1/categories.php"
+
+fetch(mealCatagoriesApiURL)
 }
+const handleFormInputFocus = () => {
+console.log('focus occurred');
 
-const searchTermsInputs = document.body.querySelector("#search-terms")
+getMealCategories();
+};
 
-document.body.addEventListener('focus', handleFormInputFocus)
+document.body.addEventListener('focus', handleFormInputFocus);
